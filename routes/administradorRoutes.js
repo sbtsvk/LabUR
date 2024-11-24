41a15865bc21e7f63215fd2a4e1c93a03b78da20
sbtsvk <149413733+sbtsvk@users.noreply.github.com>
2024-11-24 15:48:38 -0500

    Delete routes/administradorRoutes.js

--- routes/administradorRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { crearDatos, obtenerDatos } from "../controller/administradorController.js";
-const router = Router()
-
-router.get('/administradores', obtenerDatos)
-router.post('/administradores', crearDatos)
-
-
-export default router;
